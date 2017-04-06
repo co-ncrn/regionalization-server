6e675b3af73d150623e36cae1f246aa89e639604
Owen Mundy <omundy@gmail.com>
2017-04-06 13:23:51 -0400

    Adds descriptions to routes

--- routes.js
+++ routes.js
@@ -5,6 +5,7 @@
 'use strict';
 
 const Handlers = require('./handlers/db_calls.js');
+const Joi = require('joi');
 
 let path = "/api";	// original path, before server config
 // may not need to reference /api in URL while using Apache/PHP proxy
@@ -15,35 +16,61 @@ const cache = { privacy: 'private', expiresIn: 86400 * 1000 }
 // export routes to server
 module.exports = [
 	{
-		// root / test	
+		config: { 
+			description: "The root / test route",
+			tags: ['root'],
+			notes: ['For testing'],
+			security: true 
+		},
 		method: 'GET',					
 		path: '/',
-		handler: Handlers.root,
-		config: { security: true }
+		handler: Handlers.root
 	},{
 		// get all msa data
+		config: { 
+			description: "Returns all data for specified msa/scenario/data",
+			tags: ['data'],
+			notes: ['notes here'],
+			security: true,
+			cache: cache,
+			validate: {
+				params: {
+					msa: Joi.number().required().min(10180).max(49740),
+					scenario: Joi.string().required(),
+					data: Joi.string().required()
+				}
+			}
+		},
 		method: 'GET',
 		path: path + '/{msa}/{scenario}/{data}',
-		handler: Handlers.get_MSA_scenario_data,
-		config: {
-			security: true,
-			cache: cache
-		}
+		handler: Handlers.get_MSA_scenario_data
 	},{
-		// get _metadata for menus
+		config: { 
+			description: "Returns _metadata for building menus, etc.",
+			tags: ['metadata'],
+			notes: ['notes here'],
+			security: true,
+			cache: cache,
+			validate: {
+				params: {
+					msa: Joi.number().min(10180).max(49740)
+				}
+			}
+		},
 		method: 'GET',
 		path: path + '/_metadata/{msa?}',
-		handler: Handlers.get_metadata,
-		config: {
-			security: true,
-			cache: cache
-		}
+		handler: Handlers.get_metadata
+		
 	},{
-		// default
+		config: { 
+			description: "The default / catchAll route",
+			tags: ['default'],
+			notes: ['Will catch eerything that has less|more than 3 params'],
+			security: true 
+		},
 		method: 'GET',
 		path: path + '/{path*}',
-		handler: Handlers.catchAll,
-		config: { security: true }
+		handler: Handlers.catchAll
 	}
 
 
