4106d02e3cb42afb89a45520446bd81a19a94016
Owen Mundy <omundy@gmail.com>
2017-04-06 10:51:38 -0400

    Update routes to remove /apo

--- routes.js
+++ routes.js
@@ -6,47 +6,42 @@
 
 const Handlers = require('./handlers/db_calls.js');
 
+let path = "/api";	// original path, before server config
+// may not need to reference /api in URL while using Apache/PHP proxy
+path = "";	// after server config
+
+const cache = { privacy: 'private', expiresIn: 86400 * 1000 }
 
 // export routes to server
 module.exports = [
 	{
-		// root / test
-		method: 'GET',		
+		// root / test	
+		method: 'GET',					
 		path: '/',
 		handler: Handlers.root,
 		config: { security: true }
 	},{
 		// get all msa data
 		method: 'GET',
-		//path: '/api/{msa}/{scenario}/{data}', // original path, before server config
-		path: '/{msa}/{scenario}/{data}',
+		path: path + '/{msa}/{scenario}/{data}',
 		handler: Handlers.get_MSA_scenario_data,
 		config: {
 			security: true,
-			cache: {
-				privacy: 'private',
-				expiresIn: 86400 * 1000
-			}
+			cache: cache
 		}
 	},{
 		// get _metadata for menus
 		method: 'GET',
-		//path: '/api/_metadata/{msa?}',
-		path: '/_metadata/{msa?}',
+		path: path + '/_metadata/{msa?}',
 		handler: Handlers.get_metadata,
-		config: { security: true }
+		config: {
+			security: true,
+			cache: cache
+		}
 	},{
-		// may not need to reference /api in URL while using Apache/PHP proxy
-		/*
-		// catch alls 
-		method: 'GET',
-		path: '/api/{path*}',
-		handler: Handlers.catchAll_api,
-		config: { security: true }
-	},{*/
 		// default
 		method: 'GET',
-		path: '/{path*}',
+		path: path + '/{path*}',
 		handler: Handlers.catchAll,
 		config: { security: true }
 	}
