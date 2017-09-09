92cbaa7e25882745039f42f67afa0f7e614c7c7a
Owen Mundy <omundy@gmail.com>
2017-09-09 05:09:37 -0400

    Adds decimal rounding to SQL join

--- handlers/db_calls.js
+++ handlers/db_calls.js
@@ -89,16 +89,17 @@ exports.get_MSA_scenario_data = function(request, reply) {
 	WHERE t.TID = c.TID AND r.RID = c.RID
 	ORDER BY RID;
 	*/
+	var round = 5;
 	var sql = 'SELECT t.TID, c.RID, '+
-					't.'+data+'E as t_'+data+'E, r.'+data+'E as r_'+data+'E, ' +
-					't.'+data+'M as t_'+data+'M, r.'+data+'M as r_'+data+'M, ' + 
-					't.'+data+'CV as t_'+data+'CV, r.'+data+'CV as r_'+data+'CV ' +
+					'ROUND(t.'+data+'E,'+round+') as t_'+data+'E, ROUND(r.'+data+'E,'+round+') as r_'+data+'E, ' +
+					'ROUND(t.'+data+'M ,'+round+')as t_'+data+'M, ROUND(r.'+data+'M,'+round+') as r_'+data+'M, ' + 
+					'ROUND(t.'+data+'CV,'+round+') as t_'+data+'CV, ROUND(r.'+data+'CV,'+round+') as r_'+data+'CV ' +
 				'FROM '+ m_s +'_input_tracts t, '+
 				    m_s +'_output_regions r, '+
 				    m_s +'_crosswalk c ' +
 				'WHERE t.TID = c.TID AND r.RID = c.RID ' +
 				'ORDER BY RID;';
-	//console.log("sql: ",sql);
+	console.log("sql: ",sql);
 
 	// perform query
 	this.db.query(sql, function (error, results, fields) {
